e5a0cf2483d629f0707f9c12b3cfee1fac9b5a8d
Leandro Magalhaes <le.fatecpd@gmail.com>
2025-03-19 16:06:32 +1100

    Add MacOS -> {Calendar, Mail} configuration

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -139,9 +139,58 @@ defaults write com.apple.finder WarnOnEmptyTrash -bool false
 defaults write com.apple.finder FXInfoPanesExpanded -dict General -bool true OpenWith -bool true Privileges -bool true
 
 ###############################################################################
+# Mail                                                                        #
+###############################################################################
+
+# Display emails in threaded mode
+defaults write com.apple.mail DraftsViewerAttributes -dict-add "DisplayInThreadedMode" -string "yes"
+
+# Disable send and reply animations in Mail.app
+defaults write com.apple.mail DisableReplyAnimations -bool true
+defaults write com.apple.mail DisableSendAnimations -bool true
+
+# Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
+defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+
+# Disable inline attachments (just show the icons)
+defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
+
+# Disable automatic spell checking
+defaults write com.apple.mail SpellCheckingBehavior -string "NoSpellCheckingEnabled"
+
+# Disable sound for incoming mail
+defaults write com.apple.mail MailSound -string ""
+
+# Disable sound for other mail actions
+defaults write com.apple.mail PlayMailSounds -bool false
+
+# Mark all messages as read when opening a conversation
+defaults write com.apple.mail ConversationViewMarkAllAsRead -bool true
+
+# Disable includings results from trash in search
+defaults write com.apple.mail IndexTrash -bool false
+
+# Automatically check for new message (not every 5 minutes)
+defaults write com.apple.mail AutoFetch -bool true
+defaults write com.apple.mail PollTime -string "-1"
+
+# Show most recent message at the top in conversations
+defaults write com.apple.mail ConversationViewSortDescending -bool true
+
+###############################################################################
+# Calendar                                                                    #
+###############################################################################
+
+# Show week numbers (10.8 only)
+defaults write com.apple.iCal "Show Week Numbers" -bool true
+
+# Week starts on monday
+defaults write com.apple.iCal "first day of week" -int 1
+
+###############################################################################
 # Kill affected applications                                                  #
 ###############################################################################
 
-for app in "Finder" "SystemUIServer"; do
+for app in "Calendar" "Mail" "Finder" "SystemUIServer"; do
   killall "${app}" &> /dev/null
 done
